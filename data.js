e072377ef8e0bdc86189f71eb51bf94afb38c489
sherzader <sheri@hipmunk.com>
2018-11-08 14:00:02 -0800

    Add data object for chart and map

---
+++ data.js
@@ -0,0 +1,201 @@
+const dataArray = [
+    /**
+     * month: String
+     * cities: [
+     *      City: {
+     *          coords: [Number, Number],
+     *          price: Number
+     *      }
+     * ],
+     * averagePrice: Number,
+     *
+     * coords are left and top
+     */
+    {
+        month: "jan",
+        cityMarkers: [
+            {
+                city: "Austin",
+                coords: [371, 405],
+                price: 291,
+            },
+            {
+                city: "Baltimore",
+                coords: [684, 204], // 671, 182
+                price: 258,
+            },
+        ],
+        averagePrice: 291,
+    },
+    {
+        month: "feb",
+        cityMarkers: [
+            {
+                city: "San Francisco",
+                coords: [18, 205],
+                price: 273,
+            },
+        ],
+        averagePrice: 336,
+    },
+    {
+        month: "mar",
+        cityMarkers: [
+            {
+                city: "Austin",
+                coords: [],
+                price: [],
+            },
+            {
+                city: "Atlanta",
+                coords: [582, 316],
+                price: [244],
+            },
+        ],
+        averagePrice: 389,
+    },
+    {
+        month: "apr",
+        cityMarkers: [
+            {
+                city: "Austin",
+                coords: [],
+                price: [],
+            },
+            {
+                city: "Atlanta",
+                coords: [582, 316],
+                price: [244],
+            },
+        ],
+        averagePrice: 365,
+    },
+    {
+        month: "may",
+        cityMarkers: [
+            {
+                city: "Aspen",
+                coords: [230, 137],
+                price: 487,
+            },
+        ],
+        averagePrice: 375,
+    },
+    {
+        month: "jun",
+        cityMarkers: [
+            {
+                city: "Austin",
+                coords: [],
+                price: [],
+            },
+            {
+                city: "Atlanta",
+                coords: [582, 316],
+                price: [244],
+            },
+        ],
+        averagePrice: 417,
+    },
+    {
+        month: "jul",
+        cityMarkers: [
+            {
+                city: "Austin",
+                coords: [],
+                price: [],
+            },
+            {
+                city: "Atlanta",
+                coords: [582, 316],
+                price: [244],
+            },
+        ],
+        averagePrice: 424,
+    },
+    {
+        month: "aug",
+        cityMarkers: [
+            {
+                city: "Austin",
+                coords: [],
+                price: [],
+            },
+            {
+                city: "Atlanta",
+                coords: [582, 316],
+                price: [244],
+            },
+        ],
+        averagePrice: 378,
+    },
+    {
+        month: "sep",
+        cityMarkers: [
+            {
+                city: "Albuquerque",
+                coords: [234, 293],
+                price: 345,
+            },
+            {
+                city: "Atlanta",
+                coords: [582, 316],
+                price: 244,
+            },
+            {
+                city: "Boise",
+                coords: [139, 122],
+                price: 244,
+            },
+        ],
+        averagePrice: 320,
+    },
+    {
+        month: "oct",
+        cityMarkers: [
+            {
+                city: "Austin",
+                coords: [],
+                price: [],
+            },
+            {
+                city: "Atlanta",
+                coords: [582, 316],
+                price: [244],
+            },
+        ],
+        averagePrice: 321,
+    },
+    {
+        month: "nov",
+        cityMarkers: [
+            {
+                city: "Austin",
+                coords: [],
+                price: [],
+            },
+            {
+                city: "Atlanta",
+                coords: [582, 316],
+                price: [244],
+            },
+        ],
+        averagePrice: 337,
+    },
+    {
+        month: "dec",
+        cityMarkers: [
+            {
+                city: "Austin",
+                coords: [],
+                price: [],
+            },
+            {
+                city: "Atlanta",
+                coords: [582, 316],
+                price: [244],
+            },
+        ],
+        averagePrice: 355,
+    },
+];
